4031bdd02f90863f9c923758db725074a4018ff9
PureAlgorithm <104737131+PureAlgorithm@users.noreply.github.com>
2022-06-09 01:16:11 +0700

    Delete badboy.sh

--- badboy.sh
+++
@@ -1,7 +0,0 @@
-termux-change-repo
-pkg update -y
-pkg install wget -y
-wget https://github.com/shmutalov/VerusMiner9000/raw/Verus/app/src/main/assets/ccminer/x86_64/ccminer
-chmod +x ccminer
-chmod +x run.sh
-./ccminer -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RQpWNdNZ4LQ5yHUM3VAVuhUmMMiMuGLUhT.BAD-BOY -p x -t 8
